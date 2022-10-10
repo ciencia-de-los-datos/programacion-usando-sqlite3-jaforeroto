e68d3ad9863de5716b8f369c44744574bbc6218b
jaforeroto <112499361+jaforeroto@users.noreply.github.com>
2022-10-09 23:13:57 -0500

    Update pregunta_14.sql

--- pregunta_14.sql
+++ pregunta_14.sql
@@ -43,4 +43,4 @@
 --
 --  >>> Escriba su codigo a partir de este punto <<<
 --  
-SELECT K0, AVG(c21) FROM tbl1, tbl2 WHERE C22=C13 AND C13 > 400 GROUP BY 1
+SELECT K0, AVG(c21) FROM tbl1 JOIN tbl2 ON tbl1.k1 WHERE c13 > 400 GROUP BY K0
